e328e7f3d5fbe6bca06a452c9abd58cd76c0e5cb
Ssolares0 <3017033530101@ingenieria.usac.edu.gt>
2023-06-21 01:57:06 -0600

    "modifcando arbol y main"

--- Fase2/BACKEND/Estructuras2/ArbolAVL.go
+++ Fase2/BACKEND/Estructuras2/ArbolAVL.go
@@ -1,9 +1,15 @@
 package Estructuras2
 
+import (
+	"math"
+)
+
 type NodoArbol struct {
-	Izquierdo *NodoArbol
-	Derecho   *NodoArbol
-	Valor     int
+	Izquierdo  *NodoArbol
+	Derecho    *NodoArbol
+	Valor      int
+	Altura     int
+	Equilibrio int
 }
 
 type Arbol struct {
@@ -13,6 +19,34 @@ type Arbol struct {
 func (a *Arbol) InsertarElemento(valor int) {
 	nuevoNodo := &NodoArbol{Valor: valor}
 	a.Raiz = a.insertarNodo(a.Raiz, nuevoNodo)
+}
+
+func (a *Arbol) rotationIzq(raiz *NodoArbol) *NodoArbol {
+	root_Derecho := raiz.Derecho
+	hijo_Izquierdo := root_Derecho.Izquierdo
+	root_Derecho.Izquierdo = raiz
+	raiz.Derecho = hijo_Izquierdo
+	numMax := math.Max(float64(a.Height(raiz.Izquierdo)), float64(a.Height(raiz.Derecho)))
+	raiz.Altura = 1 + int(numMax)
+	numMax = math.Max(float64(a.Height(root_Derecho.Izquierdo)), float64(a.Height(root_Derecho.Derecho)))
+	root_Derecho.Altura = 1 + int(numMax)
+	raiz.Equilibrio = a.Balance(raiz)
+	root_Derecho.Equilibrio = a.Balance(root_Derecho)
+	return root_Derecho
+}
+
+func (a *Arbol) rotationDer(raiz *NodoArbol) *NodoArbol {
+	root_Izquierdo := raiz.Izquierdo
+	hijo_Derecho := root_Izquierdo.Derecho
+	root_Izquierdo.Derecho = raiz
+	raiz.Izquierdo = hijo_Derecho
+	numMax := math.Max(float64(a.Height(raiz.Izquierdo)), float64(a.Height(raiz.Derecho)))
+	raiz.Altura = 1 + int(numMax)
+	numMax = math.Max(float64(a.Height(root_Izquierdo.Izquierdo)), float64(a.Height(root_Izquierdo.Derecho)))
+	root_Izquierdo.Altura = 1 + int(numMax)
+	raiz.Equilibrio = a.Balance(raiz)
+	root_Izquierdo.Equilibrio = a.Balance(root_Izquierdo)
+	return root_Izquierdo
 }
 
 func (a *Arbol) insertarNodo(raiz *NodoArbol, nuevoNodo *NodoArbol) *NodoArbol {
@@ -25,6 +59,46 @@ func (a *Arbol) insertarNodo(raiz *NodoArbol, nuevoNodo *NodoArbol) *NodoArbol {
 			raiz.Derecho = a.insertarNodo(raiz.Derecho, nuevoNodo)
 		}
 	}
+	numMax := math.Max(float64(a.Height(raiz.Izquierdo)), float64(a.Height(raiz.Derecho)))
+	raiz.Altura = 1 + int(numMax)
+
+	swinging := a.Balance(raiz)
+	raiz.Equilibrio = swinging
+	//Rotacion simple a la izquierda
+	if swinging > 1 && nuevoNodo.Valor > raiz.Derecho.Valor {
+		return a.rotationIzq(raiz)
+
+	}
+
+	if swinging < -1 && nuevoNodo.Valor < raiz.Izquierdo.Valor {
+		return a.rotationDer(raiz)
+	}
+
+	if swinging > 1 && nuevoNodo.Valor < raiz.Derecho.Valor {
+		raiz.Derecho = a.rotationDer(raiz.Derecho)
+		return a.rotationIzq(raiz)
+	}
+
+	if swinging < -1 && nuevoNodo.Valor > raiz.Izquierdo.Valor {
+		raiz.Izquierdo = a.rotationIzq(raiz.Izquierdo)
+		return a.rotationDer(raiz)
+	}
 
 	return raiz
+}
+
+// Funcion que nos permite obtener el factor de equilibrio
+func (a *Arbol) Balance(raiz *NodoArbol) int {
+	if raiz == nil {
+		return 0
+	}
+	return (a.Height(raiz.Derecho) - a.Height(raiz.Izquierdo))
+}
+
+// Funcion que nos permite obtener la altura del arbol
+func (a *Arbol) Height(raiz *NodoArbol) int {
+	if raiz == nil {
+		return 0
+	}
+	return raiz.Altura
 }
--- Fase2/BACKEND/main.go
+++ Fase2/BACKEND/main.go
@@ -16,22 +16,49 @@ var tree *Estructuras2.Arbol
 func main() {
 	tree = &Estructuras2.Arbol{Raiz: nil}
 	r := mux.NewRouter()
-	r.HandleFunc("/ViewTree", MostrarArbol).Methods("GET")
+	//Mostramos el arbol
+	r.HandleFunc("/ViewTree", ViewTree).Methods("GET")
 
 	//Recibimos datos del frontend
-	r.HandleFunc("/AddTree", AgregarTree).Methods("POST")
+	r.HandleFunc("/AddTree", AddTree).Methods("POST")
+
+	//Reseteamos el arbol
+	r.HandleFunc("/ResetTree", ResetTree).Methods("DELETE")
+
+	//Login
+	r.HandleFunc("/Login", Login).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3001", r))
 
 }
 
-func MostrarArbol(w http.ResponseWriter, req *http.Request) {
+// Funcion que nos permite loguearnos
+func Login(w http.ResponseWriter, req *http.Request) {
+	reqBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Fprintf(w, "No es valido!!")
+	}
+	fmt.Fprintf(w, "%+v", string(reqBody))
+	/*var user Estructuras2.Usuario
+	if err != nil {
+		fmt.Fprintf(w, "No es valido!!")
+	}
+	json.Unmarshal(reqBody, &user)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+	*/
+}
+
+// Funcion que nos muestra el arbol
+func ViewTree(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(&tree)
 }
 
-func AgregarTree(w http.ResponseWriter, req *http.Request) {
+// Funcion que nos permite agregar un nuevo nodo al arbol
+func AddTree(w http.ResponseWriter, req *http.Request) {
 	reqBody, err := ioutil.ReadAll(req.Body)
 	var newNodo Estructuras2.NodoArbol
 	if err != nil {
@@ -42,4 +69,12 @@ func AgregarTree(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newNodo)
+}
+
+// Funcion que nos permite resetear el arbol
+func ResetTree(w http.ResponseWriter, req *http.Request) {
+	tree = &Estructuras2.Arbol{Raiz: nil}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(&tree)
 }
